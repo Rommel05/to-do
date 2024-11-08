c939769fa160f32c03e5d8faa64b73de5ffc6dcb
Rommel <rrommel5@gmail.com>
2024-11-08 09:48:31 +0100

    V2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     addTarea();
     removeTarea();
+    seleccionarTodo();
 }
 
 function addTarea()  {
@@ -11,21 +12,27 @@ function addTarea()  {
         let tarea = document.getElementById("tarea");
         let tareaText = tarea.value;
 
-        let divTarea = document.createElement("div");
+        if (tareaText.trim() != "") {
+            let divTarea = document.createElement("div");
 
-        let checkbox = document.createElement("input");
-        checkbox.setAttribute("type", "checkbox");
+            let checkbox = document.createElement("input");
+            checkbox.setAttribute("type", "checkbox");
 
-        let label = document.createElement("label");
-        let contentLabel = document.createTextNode(tareaText);
-        label.appendChild(contentLabel);
+            let label = document.createElement("label");
+            let contentLabel = document.createTextNode(tareaText);
+            label.appendChild(contentLabel);
 
-        divTarea.appendChild(checkbox);
-        divTarea.appendChild(label);
-        form.appendChild(divTarea);
+            divTarea.appendChild(checkbox);
+            divTarea.appendChild(label);
+            form.appendChild(divTarea);
 
-        form.reset();
-        tarea.focus();
+            form.reset();
+            tarea.focus();
+        } else {
+            alert("No puedes añadir una tarea vacia");
+            tarea.focus();
+        }
+
         
 
     })
@@ -33,24 +40,44 @@ function addTarea()  {
 
 function removeTarea() {
     let remove = document.getElementById("remove");
+    let inputTarea = document.getElementById("tarea");
 
     remove.addEventListener("click", () => {
+        let confirmar = confirm("¿Estás seguro que quieres eliminar la tarea?")
+        if (confirmar) {
+            let form = document.getElementById("form");
+
+            let divsTareas = form.getElementsByTagName("div");
+            for (let i = divsTareas.length - 1; i >= 0; i--) {
+
+                let inputs = divsTareas[i].getElementsByTagName("input");
+
+                for (let j = inputs.length - 1; j >= 0; j--) {
+                    if (inputs[j].checked) {
+                        /*let confirmar = confirm("¿Seguro que quieres eliminar la tarea?")
+                        if (confirmar) {
+                            form.removeChild(divsTareas[i]);
+                        }*/
+                    
+                        form.removeChild(divsTareas[i]);
+                        inputTarea.focus();
+                    }
+                }
+            }
+        }
+    })
+}
+
+function seleccionarTodo() {
+    let seleccionTodas = document.getElementById("seleccionarTodas");
+    seleccionTodas.addEventListener("click", () => {
         let form = document.getElementById("form");
-
         let divsTareas = form.getElementsByTagName("div");
-        for (let i = divsTareas.length - 1; i >= 0; i--) {
-
+        
+        for (let i = 0; i < divsTareas.length; i++) {
             let inputs = divsTareas[i].getElementsByTagName("input");
-            /*for (let j = 0; j < inputs.length; j++) {
-                if (inputs[j].checked) {
-                    form.removeChild(divsTareas[i]);
-                }
-            }*/
-
-            for (let j = inputs.length - 1; j >= 0; j--) {
-                if (inputs[j].checked) {
-                    form.removeChild(divsTareas[i]);
-                }
+            for (let j = 0; j < inputs.length; j++) {
+                inputs[j].checked = true;
             }
         }
     })
