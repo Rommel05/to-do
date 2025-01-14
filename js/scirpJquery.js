f5b716629bca0bff759103a1c204783a13aa9118
Rommel <rrommel5@gmail.com>
2025-01-14 10:28:04 +0100

    JQ V1

---
+++ js/scirpJquery.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    fecha();
+    addTarea();
+    removeTask();
+});
+
+function addTarea() {
+    $('#add').on('click', function () {
+        //let form = $('#form');
+        let task = $('#tarea').val();
+        let date = $('#fecha').val();
+
+        if (task.trim() != '' && date.trim() != '') {
+            $('#form').append('<div class="divTarea"></div>');
+            $('.divTarea').append('<input type="checkbox">' + '<label>'+ task + ' - ' + date + '</label>');
+
+            $('#tarea').val('');
+            $('#fecha').val('');
+
+            $('#tarea').focus();
+        } else {
+            alert("You cannot leave empty fields");
+            $('#tarea').focus();
+        }
+    });
+}
+
+function fecha() { 
+    $( function() {
+        $( "#fecha" ).datepicker({
+          showOn: "button",
+          buttonImage: "img/calendario.png",
+          buttonImageOnly: true,
+          buttonText: "Select date",
+          dateFormat: "yy-mm-dd"
+        });
+    } );
+}
+
+function removeTask() { 
+    $('#remove').on('click', function () {
+        if ($('.divTarea').length != 0) {
+            $.each($('.divTarea'), function (indexInArray, valueOfElement) { 
+                let checkbox = $(valueOfElement).find('input[type=checkbox]');
+
+                if (checkbox.is(':checked')) {
+                    $(valueOfElement).remove();
+                }
+            });
+        }
+    });
+}
