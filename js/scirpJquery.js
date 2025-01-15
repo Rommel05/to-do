acd6f9ddf3f8e318004165bc2b0c2ca6e11672ae
Rommel <rrommel5@gmail.com>
2025-01-15 12:12:29 +0100

    js_to_jquery

--- js/scirpJquery.js
+++ js/scirpJquery.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     fecha();
     addTarea();
     removeTask();
+    selectAll();
+    setInterval(time, 1000);
+    changeTheme();
 });
 
 function addTarea() {
@@ -52,6 +55,61 @@ function removeTask() {
                     $(valueOfElement).remove();
                 }
             });
+        }
+    });
+}
+
+function selectAll() {
+    $('#seleccionarTodas').on('click', function () {
+        if ($('.divTarea').length != 0) {
+            $.each($('.divTarea'), function (indexInArray, valueOfElement) { 
+                 let checkbox = $(valueOfElement).find('input[type=checkbox]');
+
+                 checkbox.prop('checked','isChecked')
+            });
+        }
+    });
+}
+
+function time() {
+    let fecha = new Date();
+    let horas = fecha.getHours();
+    let minutos = fecha.getMinutes();
+    let segundos = fecha.getSeconds();
+
+    if (horas < 10) {
+        horas = "0" + horas;
+    }
+
+    if (minutos < 10) {
+        minutos = "0" + minutos;
+    }
+
+    if (segundos < 10) {
+        segundos = "0" + segundos;
+    }
+
+    if ($('#reloj p').length == 0) {
+        $('#reloj').append('<p>'+ horas + ':' + minutos + ':'+ segundos +'</p>')
+    } else {
+        $('#reloj p').html(horas + ':' + minutos + ':'+ segundos);
+    }
+}
+
+function changeTheme() {
+    $('#cambiarTema').on('click', function () {
+        let theme = $('#cambiarTema').attr('src');
+
+        if (theme == 'img/moon.png') {
+            $('body').animate({
+                backgroundColor: '#555',
+            })
+            $('#cambiarTema').attr('src', 'img/light.png');
+        } else {
+            $('body').animate({
+                backgroundColor: 'white',
+            })
+            $('#cambiarTema').attr('src', 'img/moon.png');
         }
     });
 }
